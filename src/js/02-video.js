692c18068e08c436cadd5f47a33b081c041f63fa
Mariya-Litvin <111592916+Mariya-Litvin@users.noreply.github.com>
2023-01-29 00:00:47 +0200

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,42 @@
+// Завдання 2 - відеоплеєр
+
+// HTML містить < iframe > з відео для Vimeo плеєра.
+// Напиши скрипт, який буде зберігати поточний час відтворення
+// відео у локальне сховище і, після перезавантаження сторінки,
+//   продовжувати відтворювати відео з цього часу.
+
+// Виконуй це завдання у файлах 02 - video.html і 02 - video.js.
+// Розбий його на декілька підзавдань:
+
+// 1.Ознайомся з документацією бібліотеки Vimeo плеєра.
+// 2.Додай бібліотеку як залежність проекту через npm.
+// 3.Ініціалізуй плеєр у файлі скрипта як це описано в секції
+// pre - existing player, але враховуй, що у тебе плеєр доданий як
+// npm пакет, а не через CDN.
+// 4.Вивчи документацію методу on() і почни відстежувати подію
+// timeupdate - оновлення часу відтворення.
+// 5.Зберігай час відтворення у локальне сховище.Нехай ключем для
+//  сховища буде рядок "videoplayer-current-time".
+// 6.Під час перезавантаження сторінки скористайся методом
+// setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+// 7.Додай до проекту бібліотеку lodash.throttle і зроби так,
+//   щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
+
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function (data) {
-  duration: 61.857;
-  percent: 0.049;
-  seconds: 3.034;
-
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
-  console.log('played the video!');
+player.on('timeupdate', function (data) {
+  throttle(
+    localStorage.setItem('videoplayer-current-time', data.seconds),
+    1000
+  );
 });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const getTimeVideo = localStorage.getItem('videoplayer-current-time');
+
+if (getTimeVideo || 0) {
+  player.setCurrentTime(getTimeVideo);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,66 @@
+// Завдання 3 - форма зворотного зв'язку
 
+// HTML містить розмітку форми.Напиши скрипт, який буде
+// зберігати значення полів у локальне сховище,
+//     коли користувач щось друкує.
+
+//  Виконуй це завдання у файлах 03 - feedback.html і 03 - feedback.js.
+//   Розбий його на декілька підзавдань:
+
+// 1. Відстежуй на формі подію input, і щоразу записуй у
+// локальне сховище об'єкт з полями email і message,
+// у яких зберігай поточні значення полів форми.Нехай ключем
+// для сховища буде рядок "feedback-form-state".
+// 2. Під час завантаження сторінки перевіряй стан сховища,
+// і якщо там є збережені дані, заповнюй ними поля форми.
+// В іншому випадку поля повинні бути порожніми.
+// 3. Під час сабміту форми очищуй сховище і поля форми, а
+// також виводь у консоль об'єкт з полями email, message та
+// їхніми поточними значеннями.
+// 4. Зроби так, щоб сховище оновлювалось не частіше, ніж
+// раз на 500 мілісекунд. Для цього додай до проекту і
+// використовуй бібліотеку lodash.throttle.
+
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+setInputOnForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log({
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  });
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+function onFormInput(event) {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+
+function setInputOnForm() {
+  const savedMessage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+  if (savedMessage) {
+    form.elements.email.value = savedMessage.email;
+    form.elements.message.value = savedMessage.message;
+  }
+}
