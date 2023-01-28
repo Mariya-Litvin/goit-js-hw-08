daf263d09d9fd2923c5b0ae3b3095c78ce4f1063
Mariya-Litvin <111592916+Mariya-Litvin@users.noreply.github.com>
2023-01-28 02:39:37 +0200

    task 1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const listGallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
+    })
+    .join('');
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+listGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+listGallery.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function (data) {
+  duration: 61.857;
+  percent: 0.049;
+  seconds: 3.034;
+
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
